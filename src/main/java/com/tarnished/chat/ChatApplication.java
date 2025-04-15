d94755fb0f8d51042a696521aa5d7c8e66d34bd4
DanielPadre58 <2kupwhite@gmail.com>
2025-04-15 18:04:39 +0100

    Initial commit

---
+++ src/main/java/com/tarnished/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.tarnished.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatApplication.class, args);
+	}
+
+}
