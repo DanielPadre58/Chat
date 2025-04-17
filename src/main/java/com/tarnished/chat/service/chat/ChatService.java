027fbcafef12764b55eb036685eb4228bb26d24b
DanielPadre58 <2kupwhite@gmail.com>
2025-04-17 18:47:43 +0100

    Chat service implementation

---
+++ src/main/java/com/tarnished/chat/service/chat/ChatService.java
@@ -0,0 +1,8 @@
+package com.tarnished.chat.service.chat;
+
+import com.tarnished.chat.repository.ChatRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+}
