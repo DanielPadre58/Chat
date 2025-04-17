7900722a1aa8f753dc99c1192d0ba05d9ac7ac92
DanielPadre58 <2kupwhite@gmail.com>
2025-04-17 18:47:38 +0100

    Chat repository implementation

---
+++ src/main/java/com/tarnished/chat/repository/ChatRepository.java
@@ -0,0 +1,10 @@
+package com.tarnished.chat.repository;
+
+import com.tarnished.chat.domain.chat.Chat;
+import com.tarnished.chat.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+}
