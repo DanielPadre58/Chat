0c6d858883d0521f14c93379aa246f9518be6cab
DanielPadre58 <2kupwhite@gmail.com>
2025-04-30 14:41:04 +0100

    Chat repository update

--- src/main/java/com/tarnished/chat/repository/ChatRepository.java
+++ src/main/java/com/tarnished/chat/repository/ChatRepository.java
@@ -4,6 +4,7 @@ import com.tarnished.chat.domain.chat.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.lang.NonNull;
 
 import java.util.Optional;
 
@@ -13,4 +14,7 @@ public interface ChatRepository extends JpaRepository<Chat, Long> {
 
     @Query("SELECT c FROM Chat c LEFT JOIN FETCH c.moderators WHERE c.id = :id")
     Optional<Chat> findWithModerators(@Param("id") Long id);
+
+    @Query("select count(distinct messages) from Chat c inner join c.messages messages where messages.chat.id = ?1")
+    long getMessageCount(@NonNull Long id);
 }
